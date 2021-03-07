523ffa4685c39bdf5786688be1eb5cd32bc235ac
frontendprof <https://github.com/frontendprof>
2021-03-07 23:15:37 +0500

    searchbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import SearchBar from './components/SearchBar'
 
 const App = () => {
+
+    
     return (
-        <div>
+        <div className="ui container" style={{ marginTop:"10px" }}>
            <h2>React APp</h2> 
+           <SearchBar />
         </div>
     )
 }
