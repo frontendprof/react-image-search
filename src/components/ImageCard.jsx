d4f37066c6992a9de6e45756cf7f1da583280477
frontendprof <https://github.com/frontendprof>
2021-03-10 12:34:57 +0500

    dynamic spans

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -5,7 +5,7 @@ class ImageCard extends React.Component{
     constructor(props){
         super(props);
 
-        
+        this.state={spans:0}
 
         this.imageRef=React.createRef();
     }
@@ -15,7 +15,9 @@ class ImageCard extends React.Component{
     }
 
     setSpans=()=>{
-        console.log(this.imageRef.current.clientHeight)
+        const height=this.imageRef.current.clientHeight;
+        const spans=Math.ceil(height/10);
+        this.setState({spans})
     }
 
 
@@ -23,7 +25,7 @@ class ImageCard extends React.Component{
 
         const {urls,description}=this.props.image;
         return (
-            <div>
+            <div style={{ gridRowEnd:`span ${this.state.spans}` }}>
                 <img ref={this.imageRef} src={urls.regular} alt={description} />
                 
             </div>
